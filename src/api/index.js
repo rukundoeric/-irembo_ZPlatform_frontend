f4f10f2b5194c03b8e8856a33f55f3947bdde695
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:33:24 +0200

    feat: create src/api/index.js

---
+++ src/api/index.js
@@ -0,0 +1,16 @@
+import {
+  logIn, signUp, refreshToken,
+} from './_auth';
+import {
+  verifyUser, resentVerification, getMyProfile, passwordReset,
+} from './_user';
+
+export {
+  logIn,
+  signUp,
+  verifyUser,
+  resentVerification,
+  refreshToken,
+  getMyProfile,
+  passwordReset,
+};
