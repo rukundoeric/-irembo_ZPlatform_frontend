9c77e460299ef4f60e593e69ba9162017107ed72
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:39:16 +0200

    feat: create Splash.jsx

---
+++ src/components/shared/Splash.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Splash() {
+  return (
+    <div className="splash empty-container text-center">
+      <div className="empty-content">
+        <h3 className="mt-3">
+          DOT
+          <strong>PHARMA</strong>
+        </h3>
+      </div>
+    </div>
+  );
+}
