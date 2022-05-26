17320382c165a51904a75f948a0cc8f5f8dc57c7
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:38:16 +0200

    feat: create Icon.jsx

---
+++ src/components/shared/Icon.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+export default function Icon({ icon }) {
+  return (
+    <FontAwesomeIcon icon={icon} />
+  );
+}
