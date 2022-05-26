9c6901dd0709bb3e889be0fc466fad005a6cfeb5
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:38:27 +0200

    feat: create PasswordResetSent.jsx

---
+++ src/components/auth/PasswordResetSent.jsx
@@ -0,0 +1,70 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from 'react';
+import { Button, ProgressBar } from '../shared/Elements';
+import Loader from '../shared/Loader';
+import { resendPasswordReset } from '../../api/_user';
+
+function PasswordResetSent({
+  email,
+}) {
+  const [status, setStatus] = useState();
+  const [canResend, setCanResend] = useState(false);
+  const handleCanResend = () => {
+    setTimeout(() => {
+      setCanResend(true);
+    }, 6 * 10000);
+  };
+  const handleResend = () => {
+    if (canResend) {
+      setCanResend(false);
+      resendPasswordReset({ email }, err => {
+        if (err) {
+          setStatus('fail');
+        } else {
+          setStatus('success');
+          handleCanResend();
+        }
+      });
+    }
+  };
+
+  useEffect(() => { handleCanResend(); }, []);
+  return (
+    <div className="empty-container email-sent text-center">
+      {status === 'pending' ? (
+        <div>
+          <ProgressBar />
+          <div className="empty-content email-sent d-flex flex-column">
+            <div className="d-flex position-relative"><Loader /></div>
+          </div>
+        </div>
+      ) : (
+        <div className="empty-content">
+          <h1 className="email-sent"><i className="bi bi-envelope-paper" /></h1>
+          <h3 className="text-1 mt-3">Confirmation sent 👍</h3>
+          <small className="text-2 d-flex flex-row">
+            <span>Password reset confirmation mail was sent to</span>
+            <span className="px-2"><b>{email}</b></span>
+          </small>
+          <small className="text-2">
+            You will be receiveing the email in less than 30 seconds
+          </small>
+          <div className="f-c-link-b w-auto py-3 d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-row">
+              <span className="px-1">Did not reveive email? </span>
+            </div>
+          </div>
+          <div className="f-c-link-b w-auto py-3 d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-row">
+              <Button handleOnClick={handleResend} label="Resend" classes={`primary-button ${(!canResend || status === 'pending') && 'disabled'} mt-3`} />
+            </div>
+          </div>
+        </div>
+      ) }
+
+    </div>
+  );
+}
+
+export default PasswordResetSent;
