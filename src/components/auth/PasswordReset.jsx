26a1d58a6ba5e73dc5b21b0ecb80a160c5a20f8d
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:37:31 +0200

    feat: create PasswordReset.jsx

---
+++ src/components/auth/PasswordReset.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Empty from '../shared/Empty';
+
+export default function PasswordReset() {
+  return (
+    <div className="password-reset">
+      <Empty />
+    </div>
+  );
+}
