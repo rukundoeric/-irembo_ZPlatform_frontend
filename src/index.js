73cbcb447436ee098804724d1ff5ef81eec2059b
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:50:15 +0200

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './assets/css/style.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './views/App';
-import { store } from './app/store';
+import App from './components/App';
 import { AuthProvider } from './context/AuthProvider';
-import ContactUs from './views/communication/contactus/ContactUs';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
@@ -16,12 +13,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <Provider store={store}>
-          <ContactUs />
-          <Routes>
-            <Route path="/*" element={<App />} />
-          </Routes>
-        </Provider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>
       </AuthProvider>
     </BrowserRouter>
 
