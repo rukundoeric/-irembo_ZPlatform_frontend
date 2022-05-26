99489df231a7ed3802cad7eef4976a6ac20dcc1f
rukundoeric <ericrukundo005@gmail.com>
2022-05-26 13:33:28 +0200

    feat: create src/assets/index.js

---
+++ src/assets/index.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+/* eslint-disable import/prefer-default-export */
+import logo from './images/logo.png';
+import logoWhite from './images/logo.svg';
+import googleIcon from './images/google.png';
+import cardiology_b from './images/hc/cardiology_b.png';
+import stomatology_b from './images/hc/stomatology_b.png';
+import radiology_b from './images/hc/radiology_b.png';
+import internal_medecine_b from './images/hc/internal_medecine_b.png';
+import ophthalmology_b from './images/hc/ophthalmology_b.png';
+
+import cardiology_info from './images/hc/cardiology_info.jpeg';
+import stomatology_info from './images/hc/stomatology_info.png';
+import radiology_info from './images/hc/radiology_clinika_info.png';
+import internal_medecine_info from './images/hc/neurology_info.png';
+import ophthalmology_info from './images/hc/opthalmology_info.png';
+
+export {
+  logo,
+  googleIcon,
+  logoWhite,
+  cardiology_b,
+  stomatology_b,
+  radiology_b,
+  internal_medecine_b,
+  ophthalmology_b,
+  cardiology_info,
+  stomatology_info,
+  radiology_info,
+  internal_medecine_info,
+  ophthalmology_info,
+};
