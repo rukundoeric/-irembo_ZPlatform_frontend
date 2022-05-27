3d327c52e77278c3fb949a9937a39824e2805ba6
rukundoeric <ericrukundo005@gmail.com>
2022-05-27 02:25:05 +0200

    update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,25 +7,22 @@ import Signup from './auth/Signup';
 import ForgotPassword from './auth/ForgotPassword';
 import ApplyPasswordReset from './auth/ApplyPasswordReset';
 import NotFound from './shared/NotFound';
-// import Empty from './shared/Empty';
+import Empty from './shared/Empty';
 import Verification from './auth/VerificationComplete';
-import TermsAndConditions from './settings/TermsAndConditions';
-import PrivacyPolicy from './settings/PrivacyPolicy';
 // import Services from './home/fragments/Services';
 // import HealthTips from './home/fragments/HealthTips';
 import Layout from './layouts/Layout';
 import RequireAuth from './routes/RequireAuth';
-import RequireAuthHome from './routes/RequireAuthHome';
-import RequireAuthAdmin from './routes/RequireAuthAdmin';
-import UserHome from './home/fragments/home/UserHome';
+// import RequireAuthAdmin from './routes/RequireAuthAdmin';
+import UserHome from './pages/Home';
 
 import PersistLogin from './auth/PersistLogin';
-import Empty from './shared/Empty';
+// import Empty from './shared/Empty';
 
 // Admin components
 import Dashboard from './admin/Dashboard';
-import AdminHome from './admin/fragments/Home';
-import AdminUsers from './admin/fragments/Users';
+// import AdminHome from './admin/fragments/Home';
+// import AdminUsers from './admin/fragments/Users';
 
 function App() {
   return (
@@ -54,10 +51,10 @@ function App() {
           <Route path="/dashboard" key={key()} element={<Dashboard />}>
             {/* Private routes for admin */}
             {/* <Route element={<RequireAuthAdmin />}> */}
-            <Route element={<RequireAuthAdmin />}>
+            {/* <Route element={<RequireAuthAdmin />}>
               <Route path="/dashboard/home" key={key()} element={<AdminHome />} />
               <Route path="/dashboard/a-c-requests" key={key()} element={<AdminUsers />} />
-            </Route>
+            </Route> */}
           </Route>
 
         </Route>
