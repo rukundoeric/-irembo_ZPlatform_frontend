5f608174c0ce55304e5bf5bc13d204b48cf5f084
rukundoeric <ericrukundo005@gmail.com>
2022-05-27 02:25:59 +0200

    update _axios.js

--- src/api/_axios.js
+++ src/api/_axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE = `${process.env.REACT_APP_API_URL}`;
+// const BASE = `${process.env.REACT_APP_API_URL}`;
+const BASE = 'http://localhost:8100';
 export const axiosPrivate = axios.create({
   baseURL: BASE,
   header: { 'Content-Type': 'application/json' },
