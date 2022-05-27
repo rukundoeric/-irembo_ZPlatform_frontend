2f80e5698d581217b91468b3dcb9436b6795acb8
rukundoeric <ericrukundo005@gmail.com>
2022-05-27 02:26:08 +0200

    update _constants.js

--- src/helpers/_constants.js
+++ src/helpers/_constants.js
@@ -1,14 +1,25 @@
 export default {
-  login_api: '/user/login',
-  refresh_token_api: '/user/refresh-token',
-  admin_login_api: '/user/admin/login',
+  // auth
+  login_api: '/auth/login',
+  second_factor_auth: '/auth/second-factor-auth',
+  refresh_token_api: '/auth/refresh-token',
+  logout_api: '/auth/logout',
+
+  // User
   signup_api: '/user/signup',
-  logout_api: 'user/logout',
-  get_all_users_api: '/user/allusers',
+  verify_user_api: session => `/user/verify-email/${session}`,
+  resend_verification_api: '/user/resend-email-verification',
+
   r_psw_rese_api: '/user/request-password-reset',
-  r_r_psw_rese_api: '/user/resend-password-reset',
-  apply_psw_reset_api: session => `/user/apply-password-reset/${session}`,
-  verify_user_api: session => `/user/activateuser/${session}`,
-  resend_verification_api: '/user/resendverification',
-  my_profile_api: '/user/myprofile',
+  apply_psw_reset_api: session => `/user/reset-password/${session}`,
+  r_r_psw_rese_api: '/user/resend-password-reset-confirmation',
+
+  // Profile
+  my_profile_api: '/user/profile',
+  update_profile: '/update-profile',
+  r_account_verification: '/account-verification',
+
+  // for admin
+  get_a_v_requests: '/get-verify-account-requests',
+  verify_account: '/verify-account/:request_id',
 };
