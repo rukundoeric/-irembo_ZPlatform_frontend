edae91ae7a0c15e687b14a8eaeb698369c5ce7ed
rukundoeric <ericrukundo005@gmail.com>
2022-05-30 19:13:42 +0200

    update Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,16 +1,24 @@
 /* eslint-disable react/style-prop-object */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
-import { Outlet } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from './Header';
 import Path from '../shared/Path';
 
 export default function Dashboard() {
+  const location = useLocation();
+  const [path, setPath] = useState();
+
+  useEffect(() => {
+    const pathname = location?.pathname;
+    const newPath = [{ name: 'Home', path: '/' }, { name: pathname.split('/').join(' '), path: pathname }];
+    setPath(newPath);
+  }, [location]);
   return (
     <main className="d-flex">
       <div className="sid-content flex-grow-1">
         <Header />
-        <Path />
+        <Path path={path} />
         <div className="p-3 container">
           <Outlet />
         </div>
